3c9c49a2d6874be0d02747a56a73d2c6d04e3af9
Nadia <nadiawangberg98@gmail.com>
2021-10-17 10:15:45 +0200

    Add launchfile and node that rectifies stereo ZED data
    Issue related to stereo_image_proc pointcloud still persist

--- wheel_slam/scripts/fuse_node.py
+++ wheel_slam/scripts/fuse_node.py
@@ -5,14 +5,27 @@ Loosly couples wheel odometry and pose estimates from SLAM
 import rospy
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import CameraInfo
+
 
 odom_fused_pub = rospy.Publisher('odom_fused', Odometry, queue_size=1)
+right_pub = rospy.Publisher("left/camera_info", CameraInfo, queue_size=1)
+
 
 def wheel_odom_callback(odom_msg):
     print("got wheel odom msg")
     global odom_fused_pub
     #rospy.loginfo(rospy.get_caller_id() + "I heard %s", odom_msg.data)
     odom_fused_pub.publish(odom_msg)
+
+def fix_camera_info_right_cb(msg):
+
+    global right_pub
+    #ADD HEIGHT AND WIDTH DATA
+    #msg.height = 376
+    #msg.width = 672
+
+    right_pub.publish(msg)
 
 def slam_odom_callback(odom_msg):
     print("got slam msg")
@@ -22,6 +35,7 @@ def fuse_node():
 
     rospy.Subscriber("wheel_odom", Odometry, wheel_odom_callback)
     rospy.Subscriber("slam_odom", Odometry, slam_odom_callback)
+    rospy.Subscriber("zed/right/camera_info", CameraInfo, fix_camera_info_right_cb)
 
     rospy.spin()
 
